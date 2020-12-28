568157212a45af4ac61cf152f5abe6667d81d4b9
Greg Martin <gregm31@live.com>
2020-12-29 09:11:00 +1100

    Grouped the elements

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const canvas = d3.select(".canvas")
 
 const svg = canvas.append("svg").attr("height", 600).attr("width", 600)
 
+const group = svg.append("g").attr("transform", "translate(50, 100)")
+
 // append shapes to svg container
 
-svg
+group
   .append("rect")
   .attr("width", 200)
   .attr("height", 100)
@@ -12,17 +14,25 @@ svg
   .attr("x", 20)
   .attr("y", 20)
 
-svg
+group
   .append("circle")
   .attr("r", 50)
   .attr("cx", 300)
   .attr("cy", 70)
   .attr("fill", "pink")
 
-svg
+group
   .append("line")
   .attr("x1", 370)
   .attr("x2", 400)
   .attr("y1", 20)
   .attr("y2", 120)
   .attr("stroke", "red")
+
+svg
+  .append("text")
+  .attr("x", 20)
+  .attr("y", 200)
+  .attr("fill", "grey")
+  .text("hello, ninjas")
+  .style("font-family", "arial")
